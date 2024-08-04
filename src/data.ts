396fb10a743292fac81ec515d5f9e674481ddaa2
grosano <grosano@quanam.com>
2024-08-04 06:55:09 -0300

    First Commit

---
+++ src/data.ts
@@ -0,0 +1,15 @@
+export const members = [
+	{
+		name : 'Onasor',
+		role : 'miembro',
+	},
+	{
+		name : 'yvng_tam',
+		role : 'lider',
+	},
+	{
+		name : 'elnoob222',
+		role : 'ex-miembro',
+	}
+
+]
